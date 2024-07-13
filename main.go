6ca79b0f06b393e2e39b61db08fc2d68242abf4a
Vincent Mercator <vmercator@protonmail.com>
2024-07-13 17:20:25 -0400

    Writing example main function

--- main.go
+++ main.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import "log"
+
+func main() {
+	log.Printf("Data Folder is %s\n", dataFolder)
+	downloadPoems("poems.json")
+	poems, err := readPoemDB("poems.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = splitPoems(poems, "poem_folder")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = readPoemFolder("poem_folder")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
