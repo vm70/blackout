6ffc733689937b7e1ccf7ea80bacde53fdd004bd
Vincent Mercator <vmercator@protonmail.com>
2024-07-13 17:20:50 -0400

    Comment out searchPoemFolder; not there yet

--- searching.go
+++ searching.go
@@ -1,30 +1,40 @@
 package main
 
 import (
-	"os"
+	"log"
 	"regexp"
-	"runtime"
 )
 
 func checkLengths() {
 }
 
-func searchPoemFolder(poemFolder string) error {
-	var numCPUs = runtime.NumCPU()
-	lengths, lengthsErr := getLengths(poemFolder)
-	if lengthsErr != nil {
-		return lengthsErr
+// func searchPoemFolder(poemFolder string) error {
+// 	var numCPUs = runtime.NumCPU()
+// 	lengths, lengthsErr := getLengths(poemFolder)
+// 	if lengthsErr != nil {
+// 		return lengthsErr
+// 	}
+//   entries, err := os.ReadDir()
+//   if
+//   for idx := 0; idx < numCPUs; idx++ {
+//
+//   }
+// 	return nil
+// }
+
+func searchingRoutine(routineID int, numCPU int, rp *regexp.Regexp, poems []Poem, poemLengths []int, maxLength int) bool {
+	for idx := routineID; idx < len(poems); idx += numCPU {
+		log.Printf("Checking Poem #%d\n", idx)
+		if canBlackout(rp, poems[idx], poemLengths[idx], maxLength) {
+			return true
+		}
 	}
-  entries, err := os.ReadDir()
-  if 
-  for idx := 0; idx < numCPUs; idx++ {
-  }
-	return nil
+	return false
 }
 
-func canBlackout(rgx regexp.Regexp, poem Poem, poemLength int, maxLength int) bool {
+func canBlackout(rp *regexp.Regexp, poem Poem, poemLength int, maxLength int) bool {
 	if poemLength > maxLength {
 		return false
 	}
-	return rgx.MatchString(poem.Text)
+	return rp.MatchString(poem.Text)
 }
