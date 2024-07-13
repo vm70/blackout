dd65a88953c02cb08b0f9eb811c030c3c31baa20
Vincent Mercator <vmercator@protonmail.com>
2024-07-13 17:20:32 -0400

    Remove TestPoemsLocation test

--- parsing_test.go
+++ parsing_test.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"runtime"
 	"testing"
 )
 
 func TestDownloadingPoems(t *testing.T) {
 	downloadPoems("poems.json")
-}
-
-func TestPoemsLocation(t *testing.T) {
-	if (runtime.GOOS[0:4] == "linux") && (dataFolder != "~/.local/share/blackout/") {
-		t.Fail()
-	}
 }
 
 func TestReadPoemDB(t *testing.T) {
