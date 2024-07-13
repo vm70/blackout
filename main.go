a5f659af6391e59a648e221f3328c09590486544
Vincent Mercator <vmercator@protonmail.com>
2024-07-13 19:09:06 -0400

    Update main.go with search routine

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
-import "log"
+import (
+	"log"
+	"regexp"
+)
+
+var blackoutRP, _ = regexp.Compile("(.*?)(b)(.*?)(l)(.*?)(a)(.*?)(c)(.*?)(k)(.*?)(o)(.*?)(u)(.*?)(t)(.*?)(p)(.*?)(o)(.*?)(e)(.*?)(m)(.*?)")
 
 func main() {
 	log.Printf("Data Folder is %s\n", dataFolder)
 	downloadPoems("poems.json")
-	poems, err := readPoemDB("poems.json")
+	dbPoems, err := readPoemDB("poems.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = splitPoems(poems, "poem_folder")
+	err = splitPoems(dbPoems, "poem_folder")
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = readPoemFolder("poem_folder")
+	poemID, err := searchPoemFolder("poem_folder", blackoutRP, 400)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("Found poem ID %d to black out\n", poemID)
+// 	for i := 0; i < 100; i++ {
+// 		log.Printf("Default %d\n", i)
+// 		time.Sleep(10 * time.Millisecond)
+// 	}
 }
